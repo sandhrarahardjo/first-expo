8ed9d8476ec7664f128f40262976db31312e4447
Sandhra Rahardjo <ailhardaisy@gmail.com>
2023-09-25 11:31:56 +0700

    feat-0001 resolve PR

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, ScrollView, TouchableOpacity, Modal } from 'react-native';
+import { styles } from './styles';
 
 const renderJumbotron = () => (
   <View style={styles.jumbotron}>
@@ -84,81 +85,6 @@ const Postings = () => {
       {renderModal(isModalDisplayed, setModalDisplay, selectedPost)}
     </View>
   );
-};
-
-const styles = {
-  container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: 'rgb(58, 62, 69)',
-  },
-  jumbotron: {
-    alignItems: 'center',
-    backgroundColor: 'darkblue',
-    height: 40,
-  },
-  display4: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  card: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: 'lightcyan',
-  },
-  cardHeader: {
-    marginBottom: 5,
-  },
-  cardBody: {
-    marginBottom: 10,
-  },
-  openButton: {
-    backgroundColor: '#007BFF',
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-  openButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  modal: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'darkgray',
-    width: 413,
-  },
-  closeButton: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    padding: 10,
-    backgroundColor: 'maroon',
-  },
-  closeButtonText: {
-    color: 'white',
-    fontSize: 20,
-  },
-  modalTitle: {
-    fontSize: 21,
-    fontWeight: 'bold',
-    backgroundColor: 'darkblue',
-    color: 'white',
-    height: 100,
-    width: 413,
-    textAlign: 'center',
-  },
-  modalBody: {
-    fontSize: 16,
-    backgroundColor: 'lightcyan',
-    width: 413,
-    textAlign: 'center',
-  },
 };
 
 export default Postings;
--- navigation/screens/ProfileScreen.js
+++ navigation/screens/ProfileScreen.js
@@ -1,103 +1,114 @@
 import React, { useState } from 'react';
-import { View, Text, Button, Image, TextInput, StyleSheet } from 'react-native';
+import { View, Text, Button, Image, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'; 
-
+import { styles } from './profile.styles'
 const EditProfileScreen = () => {
+  const defaultSocialMedia = {
+    name: '',
+    github: '',
+    linkedin: '',
+    instagram: '',
+    email: '',
+  };
+
+  const [socialMedia, setSocialMedia] = useState(defaultSocialMedia);
   const [isEditing, setIsEditing] = useState(false);
-  const [name, setName] = useState('User');
-  const [email, setEmail] = useState('example@gmail.com');
-  const [instagram, setInstagram] = useState('unregistered');
   const [profileImage, setProfileImage] = useState('https://isobarscience-1bfd8.kxcdn.com/wp-content/uploads/2020/09/default-profile-picture1.jpg');
   const [imageUrl, setImageUrl] = useState(profileImage);
-  const [github, setGithub] = useState('unregistered');
-  const [linkedin, setLinkedIn] = useState('unregistered');
+
+  const [name, setName] = useState(socialMedia.name);
+  const [github, setGithub] = useState(socialMedia.github);
+  const [linkedin, setLinkedIn] = useState(socialMedia.linkedin);
+  const [instagram, setInstagram] = useState(socialMedia.instagram);
+  const [email, setEmail] = useState(socialMedia.email);
 
   const handleEditProfile = () => {
     setIsEditing(true);
   };
 
   const handleSaveProfile = () => {
+    setSocialMedia({
+      name,
+      github,
+      linkedin,
+      instagram,
+      email,
+    });
     setProfileImage(imageUrl);
     setIsEditing(false);
   };
+
+  const _renderEditComponent = () => (
+    <>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your github"
+        value={github}
+        onChangeText={(text) => setGithub(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your linkedIn"
+        value={linkedin}
+        onChangeText={(text) => setLinkedIn(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your instagram"
+        value={instagram}
+        onChangeText={(text) => setInstagram(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter image URL"
+        value={imageUrl}
+        onChangeText={(text) => setImageUrl(text)}
+      />
+    </>
+  );
+
+  const _renderViewComponent = () => (
+    <>
+      <View style={styles.row}>
+        <Icon name="user" size={20} color="black" />
+        <Text style={styles.name}>{name}</Text>
+      </View>
+      <View style={styles.row}>
+        <Icon name="github" size={20} color="black" />
+        <Text style={styles.github}>{github}</Text>
+      </View>
+      <View style={styles.row}>
+        <Icon name="linkedin" size={20} color="black" />
+        <Text style={styles.linkedin}>{name}</Text>
+      </View>
+      <View style={styles.row}>
+        <Icon name="instagram" size={20} color="black" />
+        <Text style={styles.instagram}>{instagram}</Text>
+      </View>
+      <View style={styles.row}>
+        <Icon name="envelope" size={20} color="black" />
+        <Text style={styles.email}>{email}</Text>
+      </View>
+    </>
+  );
 
   return (
     <View style={styles.container}>
       <Image source={{ uri: profileImage }} style={styles.profileImage} />
       <Text style={styles.header}>Profile</Text>
-      {isEditing ? (
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your name"
-          value={name}
-          onChangeText={(text) => setName(text)}
-        />
-      ) : (
-        <View style={styles.row}>
-          <Icon name="user" size={20} color="black" />
-          <Text style={styles.name}>{name}</Text>
-        </View>
-      )}
-      {isEditing ? (
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your Github"
-          value={github}
-          onChangeText={(text) => setGithub(text)}
-        />
-      ) : (
-        <View style={styles.row}>
-          <Icon name="github" size={20} color="black" />
-          <Text style={styles.github}>{github}</Text>
-        </View>
-      )}
-      {isEditing ? (
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your LinkedIn"
-          value={linkedin}
-          onChangeText={(text) => setLinkedIn(text)}
-        />
-      ) : (
-        <View style={styles.row}>
-          <Icon name="linkedin-square" size={20} color="black" />
-          <Text style={styles.linkedin}>{linkedin}</Text>
-        </View>
-      )}
-      {isEditing ? (
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your Instagram account"
-          value={instagram}
-          onChangeText={(text) => setInstagram(text)}
-        />
-      ) : (
-        <View style={styles.row}>
-          <Icon name="instagram" size={20} color="black" />
-          <Text style={styles.insta}>{instagram}</Text>
-        </View>
-      )}
-      {isEditing ? (
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your email"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-      ) : (
-        <View style={styles.row}>
-          <Icon name="envelope" size={20} color="black" />
-          <Text style={styles.email}>{email}</Text>
-        </View>
-      )}
-      {isEditing ? (
-        <TextInput
-          style={styles.input}
-          placeholder="Enter image URL"
-          value={imageUrl}
-          onChangeText={(text) => setImageUrl(text)}
-        />
-      ) : null}
+      {isEditing ? _renderEditComponent() : _renderViewComponent()}
       {isEditing ? (
         <Button title="Save" onPress={handleSaveProfile} />
       ) : (
@@ -106,50 +117,5 @@ const EditProfileScreen = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: 'lightblue',
-  },
-  header: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  input: {
-    width: '80%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingLeft: 10,
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  name: {
-    fontSize: 20,
-    marginLeft: 10,
-  },
-  insta: {
-    fontSize: 20,
-    marginLeft: 10,
-  },
-  email: {
-    fontSize: 20,
-    marginLeft: 10,
-  },
-  profileImage: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    marginBottom: 20,
-  },
-});
 
 export default EditProfileScreen;
